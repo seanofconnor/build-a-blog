12635b187e3819a531b8543ae2c06c138853214b
Sean O'Connor <Sean@Seans-MacBook-Pro.local>
2017-02-23 20:21:00 -0500

    created blog post form and listings on front page

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import os
 import webapp2
 import jinja2
@@ -8,7 +7,6 @@ from google.appengine.ext import db
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                 autoescape = True)
-
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
@@ -21,30 +19,32 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
-#class Art(db.Model):
-#    title = db.StringProperty(required = True)
-#    art = db.TextProperty(required = True)
-#    created = db.DateTimeProperty(auto_now_add = True)
+class Post(db.Model):
+    title = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
 
 class MainPage(Handler):
-#    def render_front(self, title="", art="", error=""):
-#        arts = db.GqlQuery("SELECT * FROM Art ORDER BY created DESC")
-#        self.render("front.html", title=title, art=art, error=error)
+    def render_front(self, title="", content="", error=""):
+        posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC")
+
+        self.render("front.html", title=title, content=content, error=error, posts=posts)
 
     def get(self):
-        self.render("front.html")
-#    def post(self):
-#        title = self.request.get("title")
-#        art = self.request.get("art")
-#
-#        if title and art:
-#            a = Art(title = title, art = art)
-#            a.put()
-#
-#            self.redirect("/")
-#        else:
-#            error = "we need both a title and some art"
-#            self.render_front(title, art, error)
+        self.render_front()
 
+    def post(self):
+        title = self.request.get("title")
+        content = self.request.get("content")
+
+        if title and content:
+            a = Post(title = title, content = content)
+            a.put()
+            self.redirect("/")
+
+        else:
+            error = "We need both a title and some content."
+            self.render_front(error = error, title = title, content = content)
 
 app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
---
+++ solution
@@ -0,0 +1,60 @@
+import os
+import re
+from string import letters
+
+import webapp2
+import jinja2
+
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                                autoescape = True)
+
+class BlogHandler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class MainPage(Handler):
+    def get(self):
+        self.render("front.html")
+
+### blog stuff
+
+def blog_key(name = 'default'):
+    return db.Key.from_path('blogs', name)
+
+class Post(db.Model):
+    subject = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(required = True)
+    last_modified = db.DateTimeProperty(auto_now = True)
+
+    def render(self):
+        self._render_text = self.content.replace('\n', '<br>')
+        return render_str("post.html", p = self)
+
+class BlogFront(BlogHandler):
+    def get(self):
+        posts = greetings = Post.all().order('-created')
+        self.render('front.html', posts = posts)
+
+class PostPage(BlogHandler):
+    def get(self, post_id):
+        key = db.Key.from_path('Post', int(post_id), parent = blog_key())
+        post = db.get(key)
+
+        if not post:
+            self.error(404)
+            return
+
+        self.render("permalink.html", post = post)
+
+app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
